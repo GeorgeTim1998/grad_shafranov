0b62c6dd3ae1c71d5bcf0d1fd061358ee289764b
georgetim1998 <georgetim1998@gmail.com>
2021-11-15 12:48:03 +0300

    papa helped

--- GS_Soloviev_psi.py
+++ GS_Soloviev_psi.py
@@ -1,5 +1,5 @@
 #%% imports
-## in terminal do 'conda start phenics project'
+## in terminal do 'source /home/george/anaconda3/bin/activate' then 'conda activate fenicsproject'
 # comment lines: cntr+K+C
 # uncomment lines: cntr+K+U
 
