829fc5d2c1774a1f0ab27ac7e5017775ea623659
georgetim1998 <georgetim1998@gmail.com>
2021-11-22 19:27:19 +0300

    cleaned up code a bit

--- GS_psi_input.py
+++ GS_psi_input.py
@@ -6,19 +6,42 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure, gcf, title
 import datetime
 import sympy
+#%% Functions
+def Save_figure(addition):
+    # Plot solution and mesh. Save plot
+    #nothing passed to function, because variables are global
+    if plot_mesh == 1:
+        plot(mesh)
+
+    mesh_title = str(mesh_r) + 'x' + str(mesh_z) + ' mesh'
+    
+    ttime = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
+    time_title = str(ttime)  #get current time to make figure name unique
+
+    #create a path to save my figure to. For some reason now I cant save using relative path
+    path_my_file = '/home/george/Projects2/Projects/Figures/Analytical/' + time_title
+
+    if addition == '_notitle':
+        plt.savefig(path_my_file + addition + '.png', dpi = dpi) #no title figure for reports
+    elif addition == '_title':
+        plt.title('Analyt Soloviev: ' + mesh_title + "\n" + f_expr._cppcode) # titled figure for my self
+        plt.savefig(path_my_file + addition + '.png', dpi = dpi)
+    else:
+        plt.title(addition) # titled figure for my self
+        plt.savefig(path_my_file + addition + '.png', dpi = dpi)
 #%% paremeters, mesh, V, u definition
-mesh_r = 10 #mesh
-mesh_z = 10
+mesh_r, mesh_z = 100, 100 # mesh for r-z space
+area = [0, 1, -1, 1] # format is: [r1, r2, z1, z2]
+rect_low = Point(area[0], area[2]) #define rectangle size: lower point
+rect_high = Point(area[1], area[3]) #define rectangle size: upper point
+
 plot_mesh = 0 #choose whether to plot mesh or not
-rectangle_low = Point(0, -1) #define rectangle size
-rectangle_high = Point(1, 1)
-
-mesh = RectangleMesh(rectangle_low, rectangle_high, mesh_r, mesh_z) # points define domain size [0, -1]x[1, 1]
-V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
-u = Function(V) # for non linear equations 'u' must be defined via Function() 
-
 save_NoTitle = 0 #save figure that doesnt have title in it
 dpi = 200 # saved figures quality
+
+mesh = RectangleMesh(rect_low, rect_high, mesh_r, mesh_z) # points define domain size [0, -1]x[1, 1]
+V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
+u = Function(V) # for non linear equations 'u' must be defined via Function() 
 #%% Equation's right hand definition 
 # expression is inverced because f deined as -f0 (what you see in GS equation)
 #!!!calc deriviation using sympy
@@ -35,7 +58,7 @@ f_text = 4 * pi * pow(x, 2) * dp_psi + F_psi*dF_psi #right hand expression
 #f_text = 4 * pi * pow(x, 2) * psi + 1 #right hand expression
 f_text = sympy.printing.ccode(f_text)
 print("\n" + f_text + "\n")
-f = Expression(f_text, u = u, degree = 2)
+f_expr = Expression(f_text, u = u, degree = 2)
 #%% Create problem
 #  mesh, u and V defined above
 
@@ -49,10 +72,10 @@ bc = DirichletBC(V, u_D, boundary) #гран условие как в задач
 
 # Define variational problem
 v = TestFunction(V)
-r2weigth = interpolate(Expression('x[0]*x[0]', degree = 2), V) # comment in {}
+w = interpolate(Expression('x[0]*x[0]', degree = 2), V) # comment in {}
 
-a = dot( grad(u)/r2weigth, grad(r2weigth*v) )*dx
-L = f*v*dx
+a = dot( grad(u)/w, grad(w*v) )*dx
+L = f_expr*v*dx
 
 # Compute solution
 solve(a - L == 0, u, bc)
@@ -69,7 +92,7 @@ path_my_file = '/home/george/Projects2/Projects/Figures/' + time_title
 
 if save_NoTitle != 0:
     plt.savefig(path_my_file + '_notitle.png', dpi = dpi) #no title figure for reports
-plt.title('Psi_input: ' + mesh_title + "\n" + f._cppcode) # titled figure for my self
+plt.title('Psi_input: ' + mesh_title + "\n" + f_expr._cppcode) # titled figure for my self
 plt.savefig(path_my_file + '_title.png', dpi = dpi)
 
 # Save solution to file in VTK format
