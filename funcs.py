576011791c33d796297ee1f74e02d2a16c3b9b64
georgetim1998 <georgetim1998@gmail.com>
2021-12-15 00:49:27 +0300

    kinda finished 2dplot

--- funcs.py
+++ funcs.py
@@ -1,13 +1,12 @@
 from __future__ import print_function
-from math import degrees
 from fenics import *
-import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure, gcf, isinteractive, title
+import matplotlib.pyplot as matplt
+# from matplotlib.pyplot import figure, gcf, isinteractive, title
 import datetime
 import numpy 
 import sympy
 from termcolor import colored
-import pylab as plt
+import pylab
 
 def Form_f_text(A1, A2):
     #A1 = 4*mo*p', A2 = FF'
@@ -17,3 +16,21 @@ def Form_f_text(A1, A2):
     print(colored("INVERCED right-hand equation side: \n", 'magenta') + f_text)
 
     return f_text
+
+def Twod_plot_(psi, x0, y1, y2): 
+    # y1, y2 - min and max points in an interval of interest, x0 - point along which 2d graph is plotted
+    tol, point_num = 0.001, 100 + 1  # avoid hitting points outside the domain
+    
+    y = numpy.linspace(y1 + tol, y2 - tol, point_num)
+    
+    points = [(x0, y_) for y_ in y]  # create 2D points
+    psi_line = numpy.array([psi(point) for point in points])
+    
+    matplt.plot(y, psi_line, 'k', linewidth=2)  # magnify w
+    # matplt.grid(True)
+    matplt.xlabel('$r$')
+    matplt.ylabel('$psi$')
+    # matplt.legend(['Deflection ($\\times 50$)', 'Load'], loc='upper left')
+    
+    matplt.savefig('poisson_membrane/curves.png')
+    matplt.close() # close created plot
