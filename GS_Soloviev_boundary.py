e0b7b1456997496f133853d623c21c2c1581d71f
georgetim1998 <georgetim1998@gmail.com>
2021-12-22 15:55:19 +0300

    succesful write 2d plot to file

--- GS_Soloviev_boundary.py
+++ GS_Soloviev_boundary.py
@@ -52,7 +52,7 @@ solve(a == L, u, bc)
 fu.What_time_is_it(t0, 'Variational problem solved')
 #%% Post analytica
 # fu.Write2file_umax_vs_def_mesh(mesh_r, mesh_z, fu.Twod_plot(u, r0, z1, z2, PATH))
-fu.Write2file_umax_vs_square_size(mesh_r, mesh_z, fu.Twod_plot(u, r0, z1, z2, PATH), fu.DEFAULT_MESH)
+fu.Write2file_umax_vs_square_size(mesh_r, mesh_z, fu.Twod_plot(u, r0, z1, z2, PATH, square), fu.DEFAULT_MESH)
 fu.What_time_is_it(t0, "Cross section plotted through r0 = %s" % r0)
 
 fig = plot(u) # its fenics' plot not python's
