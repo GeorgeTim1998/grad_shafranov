940b028317ed43ae49ab3f7e2e0548433c21d8a1
georgetim1998 <georgetim1998@gmail.com>
2022-01-24 01:49:08 +0300

    cleaned up code

--- GS_MEPhIST.py
+++ GS_MEPhIST.py
@@ -11,16 +11,11 @@ default_mesh = fu.DEFAULT_MESH
 r1, z1 = 0, -0.4 # see Krat's unpublishet article
 r2, z2 = 0.6, 0.4
 area = [r1, r2, z1, z2] # format is: [r1, r2, z1, z2]
-
 mesh_r, mesh_z = default_mesh, abs(int(default_mesh * (z2-z1)/(r2-r1)))
-
 rect_low = Point(area[0], area[2]) #define rectangle size: lower point
 rect_high = Point(area[1], area[3]) #define rectangle size: upper point
-
-A1, A2 = 0.14, 0.01 # values from Ilgisonis2016, 244
-f_text = fu.Form_f_text(A1, A2) # form right hand side that corresponds to analytical solution
-
 mesh = RectangleMesh(rect_low, rect_high, mesh_r, mesh_z) # points define domain size rect_low x rect_high
+
 V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
 u_D = Expression('0', degree = 1) # Define boundary condition
 
@@ -31,26 +26,21 @@ fu.What_time_is_it(t0, 'Variational problem solved')
 
 bc = DirichletBC(V, u_D, boundary) #гран условие как в задаче дирихле
 
-u = TrialFunction(V)
+u = Function(V) # u must be defined as function before expression def
 v = TestFunction(V)
 
-f_expr = Expression(f_text, degree = 2)
+f_text = fu.Hand_input()
+f_expr = Expression(f_text, u = u, degree = 2)
 point_sources = fu.Array_Expression(fu.ArrayOfPointSources(psd.PointSource()))
 
 r_2 = interpolate(Expression('x[0]*x[0]', degree = 2), V) # interpolation is needed so that 'a' could evaluate deriviations and such
 r = Expression('x[0]', degree = 1) # interpolation is needed so that 'a' could evaluate deriviations and such
 
-a = dot(grad(u)/r, grad(r_2*v))*dx
-L = sum(point_sources)*r*v*dx
-
 print(colored("Default mesh = %d\n" % (default_mesh), 'green'))
 
-u = Function(V)
+a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*dx
+L = sum(point_sources)*r*v*dx
 solve(a == L, u, bc)
-fu.What_time_is_it(t0, 'Variational problem solved')
-# %% 
-# fu.Write2file_umax_vs_def_mesh(mesh_r, mesh_z, fu.Twod_plot(u, r0, z1, z2, PATH))
-fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, f_expr, [mesh_r, mesh_z], plot_title)
+
+fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], '')
 fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
-# vtkfile = File('poisson/solution.pvd') # Save solution to file in VTK format
-# vtkfile << u
