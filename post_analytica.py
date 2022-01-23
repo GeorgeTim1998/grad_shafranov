106e7a9d1e1ea29979cec3ccda04adca1cd3eb5e
georgetim1998 <georgetim1998@gmail.com>
2022-01-23 16:43:14 +0300

    remembered

--- post_analytica.py
+++ post_analytica.py
@@ -6,9 +6,9 @@ print(colored("Date_Time is: %s" % fu.Time_name(), 'cyan'))
 # vs_def_mesh_name = 'point_source'
 # vs_square_size_name = vs_def_mesh_name
 # fu.Plot_umax_vs_def_mesh(vs_def_mesh_name)
-# # fu.Plot_umax_vs_square_size(vs_square_size_name, default_mesh_size=fu.DEFAULT_MESH)
+# fu.Plot_umax_vs_square_size(vs_square_size_name, default_mesh_size=fu.DEFAULT_MESH)
 
-# fu.Plot_2D_data_together()
+fu.Plot_2D_data_together()
 # это использовалось, чтобы строить графики сечения решения ГШ в случае точечного источника
 
 # fu.D_config(100)
