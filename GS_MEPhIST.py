cc027621e835629affadf4c827a5aa5154eb2b7b
georgetim1998 <georgetim1998@gmail.com>
2022-01-24 11:54:01 +0300

    futile attempt at adding neuman

--- GS_MEPhIST.py
+++ GS_MEPhIST.py
@@ -19,9 +19,20 @@ mesh = RectangleMesh(rect_low, rect_high, mesh_r, mesh_z) # points define domain
 V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
 u_D = Expression('0', degree = 1) # Define boundary condition
 
+# def Dirichlet_boundary(x, on_boundary):
+#     tol = 1e-10
+#     return on_boundary and (abs(x[1] - z1) < tol or abs(x[1] - z2) < tol or abs(x[0] - r2) < tol)
+
 def Dirichlet_boundary(x, on_boundary):
-    tol = 1e-14
-    return on_boundary and (abs(x[1] - z1) < tol or abs(x[1] - z2) < tol or abs(x[0] - r2) < tol)
+    return on_boundary
+
+# class DirichletBoundary(SubDomain):
+#     def inside(self, x, on_boundary):
+#         tol = 1E-10   # tolerance for coordinate comparisons
+#         return on_boundary and \
+#                (abs(x[1] - z1) < tol or abs(x[1] - z2) < tol or abs(x[0] - r2) < tol)
+
+# u0_boundary = DirichletBoundary()
 
 fu.What_time_is_it(t0, 'Variational problem solved')
 
@@ -38,10 +49,12 @@ r_2 = interpolate(Expression('x[0]*x[0]', degree = 2), V) # interpolation is nee
 r = Expression('x[0]', degree = 1) # interpolation is needed so that 'a' could evaluate deriviations and such
 
 print(colored("Default mesh = %d\n" % (default_mesh), 'green'))
-
-a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
+u = TrialFunction(V)
+a = dot(grad(u)/r, grad(r_2*v))*dx 
+# - f_expr*r*v*dx
 L = sum(point_sources)*r*v*dx
-solve(a - L== 0, u, bc)
+u = Function(V)
+solve(a == L, u, bc)
 
 fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], '')
 fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
