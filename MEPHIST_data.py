73065c9e2f26495fad427589cb06b316741701f3
georgetim1998 <georgetim1998@gmail.com>
2022-02-24 13:02:37 +0300

    added comments

--- MEPHIST_data.py
+++ MEPHIST_data.py
@@ -1,16 +1,15 @@
 class MEPhIST:
     # in SI
     def __init__(self):
-        self.R0 = 0.25
-        self.a = 0.13
-        self.k = 2
-        self.e = self.R0/self.a
+        self.R0 = 0.25 # Большой радиус плазмы
+        self.a = 0.13 # Малый радиус плазмы
+        self.k = 2 # Вытянутость плазмы
+        self.delta = 0.4 # вытянутость
+        self.e = self.R0/self.a # Аспектное соотношение
         
-        self.B0 = 1
-        self.I = 160e3
+        self.B0 = 1 # Тороидальное поле на оси
+        self.I = 160e3 # Ток плазмы
         
-        self.p_axis = 12.5e3
+        self.p_axis = 12.5e3 # Давление плазмы на оси
         
-        self.psi_axis = pow(self.R0, 2)*self.B0
-        
-        
+        self.psi_axis = pow(self.R0, 2)*self.B0 # Пси на оси
