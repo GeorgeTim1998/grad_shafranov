06156a0f1189697e4a9047fdd577f5cf395666f2
georgetim1998 <georgetim1998@gmail.com>
2022-03-09 15:40:45 +0300

    maximum iterations options

--- GS_MEPhIST.py
+++ GS_MEPhIST.py
@@ -7,7 +7,9 @@ print(colored("Date_Time is: %s" % fu.Time_name(), 'cyan'))
 PATH = 'MEPhIST'
 #%% Geometry
 default_mesh = fu.DEFAULT_MESH
-abs_tol, rel_tol = 1e-10, 8e-6 # default = 1e-10, 1e-9
+abs_tol, rel_tol = 1e-10, 1e-9 # default = 1e-10, 1e-9
+maximum_iterations = 50
+
 eps = fu.EPS # when zero maybe inf (1/r)
 r1, z1 = fu.R1 + eps, fu.Z1 # see Krat's unpublishet article
 r2, z2 = fu.R2, fu.Z2
@@ -19,7 +21,7 @@ rect_high = Point(area[1], area[3]) #define rectangle size: upper point
 mesh = RectangleMesh(rect_low, rect_high, mesh_r, mesh_z) # points define domain size rect_low x rect_high
 V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
 #%% Boundary conditions and function space V
-u_D_str = '0'
+u_D_str = '1e-3'
 u_D = Expression(u_D_str, degree = 1) # Define boundary condition
 bc = DirichletBC(V, u_D, fu.Neumann_boundary) #гран условие как в задаче дирихле
 
@@ -53,22 +55,22 @@ f_text = fu.Hand_input(p_pow, F_pow)
 u = fu.Initial_guess_for_u(u, 0)
 f_expr = Expression(f_text, u = u, degree = 2)
 #%% sources and point sources
-# L = sum(point_sources)*r*v*dx 
-# a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx - L 
-# solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
+L = sum(point_sources)*r*v*dx 
+a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx - L 
+solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol, "maximum_iterations": maximum_iterations}})
 #%% only sources
 # a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
 # solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
 #%% only point sources
-L = sum(point_sources)*r*v*dx 
-u = TrialFunction(V)
-a = dot(grad(u)/r, grad(r_2*v))*dx 
-u = Function(V)
-solve(a == L, u, bc)
+# L = sum(point_sources)*r*v*dx 
+# u = TrialFunction(V)
+# a = dot(grad(u)/r, grad(r_2*v))*dx 
+# u = Function(V)
+# solve(a == L, u, bc)
 #%% Endproblem
 fu.What_time_is_it(t0, "Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow))
 print(colored("Calculations, however bad, finished", 'green'))
 
-plot_title = "u_D = %.0e, EPS = %.0e" % (float(u_D_str), eps)
+plot_title = "%d, %d, Г = %.0e, %.1e" % (int(default_mesh), int(maximum_iterations), float(u_D_str), eps)
 fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], plot_title, 20)
 fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
