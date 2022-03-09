c5ffcd1a7e432c94120766a44ffd1da065d8637a
georgetim1998 <georgetim1998@gmail.com>
2022-03-09 14:04:49 +0300

    added new solve regime

--- GS_MEPhIST.py
+++ GS_MEPhIST.py
@@ -5,7 +5,6 @@ t0 = time.time()
 print(colored("\n---------GS_MEPhIST.py---------", 'green'))
 print(colored("Date_Time is: %s" % fu.Time_name(), 'cyan'))
 PATH = 'MEPhIST'
-plot_title = 'MEPhIST'
 #%% Geometry
 default_mesh = fu.DEFAULT_MESH
 abs_tol, rel_tol = 1e-10, 8e-6 # default = 1e-10, 1e-9
@@ -20,7 +19,7 @@ rect_high = Point(area[1], area[3]) #define rectangle size: upper point
 mesh = RectangleMesh(rect_low, rect_high, mesh_r, mesh_z) # points define domain size rect_low x rect_high
 V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
 #%% Boundary conditions and function space V
-u_D_str = '1e-1'
+u_D_str = '0'
 u_D = Expression(u_D_str, degree = 1) # Define boundary condition
 bc = DirichletBC(V, u_D, fu.Neumann_boundary) #гран условие как в задаче дирихле
 
@@ -49,21 +48,27 @@ print(colored("Default mesh = %d\n" % (default_mesh), 'green'))
 #%% SOLVING PROBLEM#2
 p_pow = 2
 F_pow = 2
+
 f_text = fu.Hand_input(p_pow, F_pow)
 u = fu.Initial_guess_for_u(u, 0)
-
 f_expr = Expression(f_text, u = u, degree = 2)
-
-L = sum(point_sources)*r*v*dx 
-a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx - L 
-solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
-
+#%% sources and point sources
+# L = sum(point_sources)*r*v*dx 
+# a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx - L 
+# solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
+#%% only sources
 # a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
 # solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
-
-fu.What_time_is_it(t0, "Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow))
+#%% only point sources
+L = sum(point_sources)*r*v*dx 
+u = TrialFunction(V)
+a = dot(grad(u)/r, grad(r_2*v))*dx 
+u = Function(V)
+solve(a == L, u, bc)
 #%% Endproblem
+fu.What_time_is_it(t0, "Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow))
 print(colored("Calculations, however bad, finished", 'green'))
 
-fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], u_D_str, 20)
+plot_title = "u_D = %.0e, EPS = %.0e" % (float(u_D_str), eps)
+fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], plot_title, 20)
 fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
