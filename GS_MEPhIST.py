47777df5ace3cf82ee7fc5051e746de8b1a45f8a
georgetim1998 <georgetim1998@gmail.com>
2022-03-23 15:54:38 +0300

    made code more readable

--- GS_MEPhIST.py
+++ GS_MEPhIST.py
@@ -1,3 +1,4 @@
+from funcs import print_colored
 from imports import *
 import time
 #%% Pre-programm stuff
@@ -14,65 +15,75 @@ eps = fu.EPS # when zero maybe inf (1/r)
 r1, z1 = fu.R1 + eps, fu.Z1 # see Krat's unpublishet article
 r2, z2 = fu.R2, fu.Z2
 area = [r1, r2, z1, z2] # format is: [r1, r2, z1, z2]
+
 mesh_r, mesh_z = default_mesh, abs(int(default_mesh * (z2-z1)/(r2-r1)))
 rect_low = Point(area[0], area[2]) #define rectangle size: lower point
 rect_high = Point(area[1], area[3]) #define rectangle size: upper point
 
 mesh = RectangleMesh(rect_low, rect_high, mesh_r, mesh_z) # points define domain size rect_low x rect_high
 V = FunctionSpace(mesh, 'P', 1) # standard triangular mesh
-#%% Boundary conditions and function space V
-u_D_str = '0'
-u_D = Expression(u_D_str, degree = 1) # Define boundary condition
-bc = DirichletBC(V, u_D, fu.Dirichlet_boundary) #гран условие как в задаче дирихле
 
 u = Function(V) # u must be defined as function before expression def
 v = TestFunction(V)
 
 r_2 = interpolate(Expression('x[0]*x[0]', degree = 2), V) # interpolation is needed so that 'a' could evaluate deriviations and such
 r = Expression('x[0]', degree = 1) # interpolation is needed so that 'a' could evaluate deriviations and such
-alpha = 2.5
-point_sources = fu.Array_Expression(fu.ArrayOfPointSources(psd.PointSource(alpha)))
+#%% Boundary conditions and function space V
+u_D_str = '0'
+u_D = Expression(u_D_str, degree = 1) # Define boundary condition
+bc = DirichletBC(V, u_D, fu.Dirichlet_boundary) #гран условие как в задаче дирихле
 
-print(colored("Default mesh = %d\n" % (default_mesh), 'green'))
-#%% SOLVING PROBLEM#1
-# p_pow = 1
-# F_pow = 2
-# f_text = fu.Hand_input(p_pow, F_pow)
+# A1 = 0.1
+# A2 = 1
+# step = 0.1
+# alpha_array = numpy.linspace(A1, A2, 1+int((A2-A1)/step))
 
-# f_expr = Expression(f_text, u = u, degree = 2)
-# a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
-# solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
-# fu.What_time_is_it(t0, "Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow))
-# fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], '', 20)
+alpha_array = [1]
 
-# fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], '', 20)
-# fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
-#%% SOLVING PROBLEM#2
-p_pow = 2
-F_pow = 2
+for alpha in alpha_array:
+    point_sources = fu.Array_Expression(fu.ArrayOfPointSources(psd.PointSource(alpha)))
 
-f_text = fu.Hand_input(p_pow, F_pow)
-u = fu.Initial_guess_for_u(u, 0)
-f_expr = Expression(f_text, u = u, degree = 2)
-fu.What_time_is_it(t0, 'Problem posted')
-#%% sources and point sources
-L = sum(point_sources)*r*v*dx 
-a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx - L 
-solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol, "maximum_iterations": maximum_iterations}})
-#%% only sources
-# a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
-# solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol, "maximum_iterations": maximum_iterations}})
-#%% only point sources
-# L = sum(point_sources)*r*v*dx 
-# u = TrialFunction(V)
-# a = dot(grad(u)/r, grad(r_2*v))*dx 
-# u = Function(V)
-# solve(a == L, u, bc)
-#%% Endproblem
-fu.What_time_is_it(t0, 'Variational problem solved')
-print(colored("Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow)))
-print(colored("Calculations, however bad, finished", 'green'))
+    fu.print_colored("Default mesh = %d\n" % (default_mesh), 'green')
+    #%% SOLVING PROBLEM#1
+    # p_pow = 1
+    # F_pow = 2
+    # f_text = fu.Hand_input(p_pow, F_pow)
 
-plot_title = "%.1e, %d, %.1e" % (alpha, int(default_mesh), eps)
-fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], plot_title, 20)
-fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
+    # f_expr = Expression(f_text, u = u, degree = 2)
+    # a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
+    # solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol}})
+    # fu.What_time_is_it(t0, "Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow))
+    # fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], '', 20)
+
+    # fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], '', 20)
+    # fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
+    #%% SOLVING PROBLEM#2
+    p_pow = 2
+    F_pow = 2
+
+    f_text = fu.Hand_input(p_pow, F_pow)
+    u = fu.Initial_guess_for_u(u, 0)
+    f_expr = Expression(f_text, u = u, degree = 2)
+    fu.What_time_is_it(t0, 'Problem posted')
+    #%% sources and point sources
+    L = sum(point_sources)*r*v*dx 
+    a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx - L 
+    solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol, "maximum_iterations": maximum_iterations}})
+    #%% only sources
+    # a = dot(grad(u)/r, grad(r_2*v))*dx - f_expr*r*v*dx
+    # solve(a == 0, u, bc, solver_parameters={"newton_solver": {"relative_tolerance": rel_tol, "absolute_tolerance": abs_tol, "maximum_iterations": maximum_iterations}})
+    #%% only point sources
+    # L = sum(point_sources)*r*v*dx 
+    # u = TrialFunction(V)
+    # a = dot(grad(u)/r, grad(r_2*v))*dx 
+    # u = Function(V)
+    # solve(a == L, u, bc)
+    #%% Endproblem
+    print("alpha = %e" % alpha)
+    fu.What_time_is_it(t0, 'Variational problem solved')
+    fu.print_colored("Solve for p_pow = %s, F_pow = %s" % (p_pow, F_pow), 'green')
+    fu.print_colored("Calculations, however bad, finished", 'green')
+
+    plot_title = "%.2e, %d, %.1e" % (alpha, int(default_mesh), eps)
+    fu.Contour_plot([r1, r2], [z1,  z2], u, PATH, '', [mesh_r, mesh_z], plot_title, 20)
+    fu.What_time_is_it(t0, "3D plot of \u03C8(r, z) is plotted")
