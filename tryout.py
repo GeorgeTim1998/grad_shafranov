1aede1742f48dff8a0e31cda0ac934e32cd0e51b
georgetim1998 <georgetim1998@gmail.com>
2022-03-31 13:42:32 +0300

    valuable info

--- tryout.py
+++ tryout.py
@@ -3,11 +3,16 @@ import fenics
 import matplotlib.pyplot as matplt
 
 a = fenics.Point(0,0)
-b = fenics.Point(1,0)
-c = fenics.Point(0.5,0.5)
+b = fenics.Point(4,0)
+c = fenics.Point(10,0.7)
+d = fenics.Point(2,5)
 
-domain = mshr.Polygon([a,b,c])
-mesh = mshr.generate_mesh(domain, 10)
+domain = mshr.Polygon([a,b,c,d])
+mesh = mshr.generate_mesh(domain, 20)
 
-fenics.plot(domain)
+fenics.plot(mesh)
 matplt.show()
+
+# *** Error:   Unable to create polygon.
+# *** Reason:  Polygon vertices must be given in counter clockwise order.
+# *** Where:   This error was encountered inside CSGPrimitives2D.cpp.
