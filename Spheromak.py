2571580ab135cb3d1ee0bb530548d2e0f56347aa
GeorgeTim1998 <georgetim1998@gmail.com>
2022-04-17 20:33:47 +0300

    done

--- Spheromak.py
+++ Spheromak.py
@@ -15,15 +15,17 @@ current_pyfile = '---------Spheromak.py---------'
 logger.log_n_output("%s" % current_pyfile, 'red')
 fu.print_colored("Date_Time is: %s" % fu.Time_name(), 'cyan')
 PATH = 'Spheromak'
+
 #%% Needed objects and contour levels
 boundary = BoundaryConditions()
 geometry = Geometry()
 mesh_density = 50
-# levels = numpy.linspace(-0.5, 0.15, 40)
+
 levels = 20
+levels = numpy.linspace(-0.14, 0.15, 25)
 
 #%% Domain and mesh definition
-geometry.rectangle_mesh_init(r1 = 0.05, r2 = 0.7, z1 = -0.6, z2 = 0.6, default_mesh = 40)
+geometry.rectangle_mesh_init(r1 = 0.05, r2 = 0.7, z1 = -0.6, z2 = 0.6, default_mesh = 80)
 fu.plot_mesh(geometry.mesh, PATH)
 # domain = fu.spheromak_bounfdary(R, delta, 500)
 # mesh = mshr.generate_mesh(domain, 50)
@@ -34,7 +36,7 @@ u = TrialFunction(V) # u must be defined as function before expression def
 v = TestFunction(V)
 
 #%% Boundary conditions
-boundary.spheromak_boundary_condition(psi_0 = 0.1, R = 0.3, alpha = 1.3)
+boundary.spheromak_boundary_condition(psi_0 = 0.1, R = 0.3, alpha = 1.1)
 u_D = boundary.psi_sol_expr
 bc = DirichletBC(V, u_D, fu.Dirichlet_boundary) #гран условие как в задаче дирихле
 
