7a6b12b0652d304f7c58ed51afffaf3efc463dd9
GeorgeTim1998 <georgetim1998@gmail.com>
2022-04-17 19:11:05 +0300

    boundary conditions class

---
+++ boundary_conditions.py
@@ -0,0 +1,36 @@
+from fenics import *
+import sympy
+import logger
+
+class BoundaryConditions:
+#%% Spheromak
+    def spheromak_boundary_condition(self, psi_0, R, alpha):
+        self.psi_0 = psi_0
+        self.R = R
+        self.alpha = alpha
+        
+        self.spheromak_sympy()
+        self.spheromak_source()
+        self.log_spheromak_boundary()
+        self.psi_sol_expr = Expression(self.spheromak_text, degree = 2)
+
+    def spheromak_sympy(self):
+        r = sympy.symbols('x[0]')
+        z = sympy.symbols('x[1]')
+        
+        symbols = self.psi_0 * pow(r, 2) / pow(self.R, 4) * ( 2*pow(self.R, 2) - pow(r, 2) - 4*pow(self.alpha, 2)*pow(z, 2) )
+        self.spheromak_text = sympy.printing.ccode(symbols)
+    
+    def spheromak_source(self):
+        self.spheromak_right_hand_expr = Expression("pow(x[0], 2) / pow(%s, 4) * (1 + pow(%s, 2)) * %s" % (self.R, self.alpha, self.psi_0), degree = 2)
+        
+        return self.spheromak_right_hand_expr
+        
+    def log_spheromak_boundary(self):
+        logger.info("psi_sol = %s" % self.spheromak_text)
+        logger.log_n_output("Rigth hand part:", 'red')
+        logger.log_n_output(self.spheromak_right_hand_expr._cppcode, 'white')
+#%% Constant boundary conditions
+    def constant_boundary_condition(self, const_str):
+        logger.info("u_D = %s" % const_str)
+        self.const_boundary = Expression(const_str, degree = 1) # Define boundary condition
