5edc45cf5c344922d7f21ea1425d26e3ac560a86
GeorgeTim1998 <georgetim1998@gmail.com>
2022-04-19 20:07:15 +0300

    logger update, tokamak D config functionality added

--- boundary_conditions.py
+++ boundary_conditions.py
@@ -30,6 +30,7 @@ class BoundaryConditions:
         
     def log_spheromak_boundary(self):
         logger.info("R = %s, alpha = %s" % (self.R, self.alpha))
+        logger.info("psi_0 = %s" % self.psi_0)
         logger.info("psi_sol = %s" % self.spheromak_text)
         logger.log_n_output("Rigth hand part:", 'red')
         logger.log_n_output(self.spheromak_right_hand_expr._cppcode, 'white')
@@ -37,3 +38,39 @@ class BoundaryConditions:
     def constant_boundary_condition(self, const_str):
         logger.info("u_D = %s" % const_str)
         self.const_boundary = Expression(const_str, degree = 1) # Define boundary condition
+#%% Tokamak D configuration
+    def tokamak_D_config_boundary_condition(self, psi_0, eps, a, b):
+        self.psi_0 = psi_0
+        self.eps = eps
+        self.a = a
+        self.b = b
+        
+        self.tokamak_D_config_sympy()
+        self.tokamak_D_config_source()
+        self.log_tokamak_D_config_boundary()
+        self.psi_sol_expr = Expression(self.tokamak_D_config_text, degree = 2)
+
+    def tokamak_D_config_sympy(self):
+        r = sympy.symbols('x[0]')
+        z = sympy.symbols('x[1]')
+        
+        symbols = self.psi_0 * (pow(r, 2) / pow(self.eps, 2) - 1) * (1 - pow(r, 2) / pow(self.a, 2) - pow(z, 2) / pow(self.b, 2))
+        self.tokamak_D_config_text = sympy.printing.ccode(symbols)
+    
+    def tokamak_D_config_source(self):
+        r = sympy.symbols('x[0]')
+        symbols = 2*self.psi_0*( (pow(2/self.a, 2) + pow(1/self.b, 2)) * pow(r/self.eps, 2) - pow(1/self.b, 2) )
+        self.tokamak_D_config_right_hand_expr = Expression(sympy.printing.ccode(symbols), degree = 2)
+        
+        return self.tokamak_D_config_right_hand_expr
+        
+    def log_tokamak_D_config_boundary(self):
+        logger.log_n_output_colored_message(colored_message="psi_0 = ", color = 'green', white_message=str(self.psi_0))
+        
+        logger.log_n_output_colored_message(colored_message="eps (ellipse) = ", color = 'green', white_message=str(self.eps))
+        logger.log_n_output_colored_message(colored_message="a = ", color = 'green', white_message=str(self.a))
+        logger.log_n_output_colored_message(colored_message="b = ", color = 'green', white_message=str(self.b))
+        
+        logger.info("psi_sol = %s" % self.tokamak_D_config_text)
+        logger.log_n_output("Rigth hand part:", 'red')
+        logger.log_n_output(self.tokamak_D_config_right_hand_expr._cppcode, 'white')
