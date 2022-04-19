57595313b7cc2aec9138b4b870aa5c5ecab87327
GeorgeTim1998 <georgetim1998@gmail.com>
2022-04-19 20:08:26 +0300

    logger update log_n_output

--- logger.py
+++ logger.py
@@ -12,6 +12,10 @@ def log_n_output(message, color):
     logging.info(message)
     print_colored(message, color)
     
+def log_n_output_colored_message(colored_message, color, white_message):
+    logging.info(colored_message + white_message)
+    print_colored_n_white(colored_text=colored_message, color=color, white_text=white_message)
+    
 def log_n_output_return_carriage(message, color):
     logging.info(message)
     print_colored(message + "\n", color)
@@ -21,3 +25,6 @@ def info(message):
     
 def print_colored(text, color):
     print(colored(text, color))
+    
+def print_colored_n_white(colored_text, color, white_text):
+    print(colored(colored_text, color) + white_text)
