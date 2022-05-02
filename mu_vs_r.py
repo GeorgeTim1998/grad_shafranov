bd5804ec99f8b7a61266746a07fd8f0ab3461007
GeorgeTim1998 <georgetim1998@gmail.com>
2022-05-03 00:50:07 +0300

    rid of extra plots

--- mu_vs_r.py
+++ mu_vs_r.py
@@ -29,11 +29,10 @@ VESSEL_PERMEABILITY = 100
 logger.log_n_output_colored_message(colored_message="VACUUM_PERMEABILITY = ", color='green', white_message=str(VACUUM_PERMEABILITY))
 logger.log_n_output_colored_message(colored_message="VESSEL_PERMEABILITY = ", color='green', white_message=str(VESSEL_PERMEABILITY))
 
-levels = 20
+levels = 80
+# levels = numpy.linspace(0, 7e-5)
 
 #%% Domain and mesh definition
-geometry.rectangle_mesh_init(r1 = 0.05, r2 = 0.55, z1 = -0.4, z2 = 0.4, default_mesh = 100) # Howell2014
-
 domain = geometry.rectangle_domain(area=[0.05, 0.55, -0.4, 0.4])
 circle1 = geometry.circle_domain(radius=0.1, segments=60)
 circle2 = geometry.circle_domain(radius=0.15, segments=100)
@@ -45,13 +44,13 @@ domain.set_subdomain(2, circle1)
 
 geometry.generate_mesh_in_domain(domain=domain, density=128)
 
-plot(geometry.mesh)
-fu.save_contour_plot(PATH, '')
+# plot(geometry.mesh)
+# fu.save_contour_plot(PATH, '')
 
 markers = MeshFunction("size_t", geometry.mesh, geometry.mesh.topology().dim(), geometry.mesh.domains())
 
-plot(markers)
-fu.save_contour_plot(PATH, 'Markers')
+# plot(markers)
+# fu.save_contour_plot(PATH, 'Markers')
 
 #%% Step coefficients classes
 class Permeability(UserExpression):
@@ -63,12 +62,14 @@ class Permeability(UserExpression):
             values[0] = VESSEL_PERMEABILITY # vessel
         else:
             values[0] = VACUUM_PERMEABILITY # vacuum
+    def value_shape(self):
+        return ()
 
 #%% Define function space and step coefficients
 V = FunctionSpace(geometry.mesh, 'P', 1) # standard triangular mesh
 
 mu = Permeability(geometry.mesh, degree=0)
-fu.countour_plot_via_mesh(geometry, interpolate(mu, V), levels = levels, PATH = PATH, plot_title = 'Permeability')
+# fu.countour_plot_via_mesh(geometry, interpolate(mu, V), levels = levels, PATH = PATH, plot_title = 'Permeability')
 
 u = TrialFunction(V) # u must be defined as function before expression def
 v = TestFunction(V)
@@ -83,23 +84,25 @@ bc = DirichletBC(V, u_D, fu.Dirichlet_boundary) #гран условие как 
 dx = Measure('dx', domain=geometry.mesh, subdomain_data=markers)
 
 point_sources = fu.Array_Expression(fu.ArrayOfPointSources(psd.PointSource(1)))
-p_correction_array = [1e-2, 1e-1, 1e1, 1e2, 1e3, 1e4]
 
-for p_correction in p_correction_array:
-    [p_coeff, F_2_coeff] = fu.plasma_sources_coefficients_pow_2(p_correction=p_correction, F_correction=1)
+# p_correction_array = [1e-2, 1e1, 1e2, 1e3, 1e4]
 
-    a = dot(grad(u)/r, grad(r_2*v))*dx - mu*(p_coeff*r*r + F_2_coeff)*u*r*v*dx(2)
-    L = mu*sum(point_sources)*r*v*dx 
+# for p_correction in p_correction_array:
+#     u = TrialFunction(V) # u must be defined as function before expression def
+[p_coeff, F_2_coeff] = fu.plasma_sources_coefficients_pow_2(p_correction=1e3, F_correction=1)
 
-    # a = dot(grad(u)/r, grad(r_2*v))*dx - (p_coeff*r*r + F_2_coeff)*u*r*v*dx(0)
-    # L = Constant(0)*r*v*dx 
+a = dot(grad(u)/r, grad(r_2*v))*dx - mu*(p_coeff*r*r + F_2_coeff)*u*r*v*dx(2)
+L = mu*sum(point_sources)*r*v*dx 
 
-    u = Function(V)
-    solve(a == L, u, bc)
+# a = dot(grad(u)/r, grad(r_2*v))*dx - (p_coeff*r*r + F_2_coeff)*u*r*v*dx(0)
+# L = Constant(0)*r*v*dx 
 
-    #%% Post solve
-    fu.What_time_is_it(t0, 'Variational problem solved')
-    fu.countour_plot_via_mesh(geometry, u, levels = levels, PATH = PATH, plot_title = '')
+u = Function(V)
+solve(a == L, u, bc)
 
-    fu.What_time_is_it(t0, "\u03C8(r, z) is plotted")
-    logger.info("'Done'"+"\n")
+#%% Post solve
+fu.What_time_is_it(t0, 'Variational problem solved')
+fu.countour_plot_via_mesh(geometry, u, levels = levels, PATH = PATH, plot_title = '')
+
+fu.What_time_is_it(t0, "\u03C8(r, z) is plotted")
+logger.log_n_output_colored_message(colored_message="'Done'\n", color='red', white_message='')
