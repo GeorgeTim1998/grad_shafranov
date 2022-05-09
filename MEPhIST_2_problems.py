c5ef6ead4bbb9bcd6b4d575d60bd024bf2971a8a
georgetim1998 <georgetim1998@gmail.com>
2022-05-09 15:59:55 +0300

    2 problem stuff

---
+++ MEPhIST_2_problems.py
@@ -0,0 +1,97 @@
+#%% Imports
+from fenics import *
+import matplotlib.pyplot as matplt
+import logger
+import mshr
+import time
+import funcs as fu
+import MEPHIST_data as M
+import logger
+from geometry import Geometry
+from boundary_conditions import BoundaryConditions
+import point_source_data as psd
+import numpy
+import MEPhIST_2_problems_problem_params as P
+    
+#%% Pre-programm stuff
+t0 = time.time()
+current_pyfile = '---------MEPhIST_2_problems.py---------'
+logger.log_n_output("%s" % current_pyfile, 'red')
+fu.print_colored("Date_Time is: %s" % fu.Time_name(), 'cyan')
+PATH = 'MEPhIST_2_problems'
+
+#%% Needed objects
+boundary_conditions = BoundaryConditions()
+geometry = Geometry()
+psi_axis = M.MEPhIST().psi_axis
+problem = P.Problem()
+
+#%% Domain and mesh definition
+domain = geometry.circle_domain(centre_point=problem.plasma_centre_point, radius=problem.plasma_radius, segments=problem.plasma_domain_segments)
+geometry.generate_mesh_in_domain(domain=domain, density=problem.mesh_density)
+
+markers = MeshFunction("size_t", geometry.mesh, geometry.mesh.topology().dim(), geometry.mesh.domains())
+
+class PlasmaStepConstant(UserExpression):
+    def __init__(self, mesh, **kwargs):
+        super().__init__(**kwargs)
+        self.markers = markers
+    def eval_cell(self, values, x, cell):
+        if self.markers[cell.index] == 1:
+            values[0] = 1 # vessel
+        else:
+            values[0] = 0 # vacuum
+    def value_shape(self):
+        return ()
+
+class VacuumStepConstant(UserExpression):
+    def __init__(self, mesh, **kwargs):
+        super().__init__(**kwargs)
+        self.markers = markers
+    def eval_cell(self, values, x, cell):
+        if self.markers[cell.index] == 1:
+            values[0] = 0 # vessel
+        else:
+            values[0] = 1 # vacuum
+    def value_shape(self):
+        return ()
+    
+etta = PlasmaStepConstant(geometry.mesh, degree=1)
+tetta = VacuumStepConstant(geometry.mesh, degree=1)
+#%% Define function space
+V = FunctionSpace(geometry.mesh, 'Lagrange', 1)
+
+u = TrialFunction(V)
+v = TestFunction(V)
+
+#%% Boundary conditions
+u_D = boundary_conditions.constant_boundary_condition(problem.boundary_condition_str)
+bc = DirichletBC(V, u_D, fu.Dirichlet_boundary)
+
+#%% Solve
+[r_2, r] = geometry.operator_weights(V)
+
+dx = Measure('dx', domain=geometry.mesh, subdomain_data=markers)
+
+point_sources = fu.Array_Expression(fu.ArrayOfPointSources(psd.PointSource(problem.point_source_disp)))
+
+# for correction in array:
+[p_coeff, F_2_coeff] = fu.plasma_sources_coefficients_pow_2_iteration(p_correction=problem.p_correction, F_correction=problem.F_correction, psi_axis=problem.psi_correction*problem.psi_axis)
+logger.log_n_output_colored_message(colored_message="Correction coeff for psi on axis = ", color='green', white_message=str(problem.psi_correction))
+
+u = TrialFunction(V)
+a = dot(grad(u)/r, grad(r_2*v))*dx + (p_coeff*r*r + F_2_coeff)*u*r*v*dx
+L = Constant(0)*r*v*dx
+# L = tetta * sum(point_sources[2:len(point_sources)])*r*v*dx
+
+u = Function(V)
+solve(a == L, u, bc)
+
+#%% Post solve
+fu.What_time_is_it(t0, 'Variational problem solved')
+fu.countour_plot_via_mesh(geometry, u, levels = problem.contour_levels, PATH = PATH, plot_title = '')
+
+fu.fenics_plot(u, PATH, plot_title='')
+
+fu.What_time_is_it(t0, "\u03C8(r, z) is plotted")
+logger.log_n_output_colored_message(colored_message="'Done'\n", color='red', white_message='')
---
+++ MEPhIST_2_problems_problem_params.py
@@ -0,0 +1,30 @@
+import numpy
+import MEPHIST_data as M
+import logger
+class Problem:
+    def __init__(self):
+        self.domain_geometry = [0.05, 0.55, -0.4, 0.4]
+        
+        self.plasma_centre_point = [0.235, 0]
+        self.plasma_radius = 0.09
+        self.plasma_domain_segments=60
+        self.point_source_disp = 1
+        
+        self.mesh_density = 180
+        
+        self.boundary_condition_str = '0.15'
+        
+        self.psi_axis = float(self.boundary_condition_str)
+        
+        self.p_correction = 1
+        self.F_correction = 1
+        self.psi_correction = 1
+        
+        self.A1 = 1e-3  
+        self.A2 = 1e-2  
+        self.step = 0.5e-3
+        self.cycle_array = numpy.linspace(self.A1, self.A2, 1+int((self.A2-self.A1)/self.step))  
+            
+        self.contour_levels = 10
+        # self.contour_levels = list(numpy.geomspace(-1e-3, 1e-8))
+        logger.log_n_output_colored_message(colored_message="point_source_disp = ", color='green', white_message=str(self.point_source_disp))
