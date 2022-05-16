09dd3e77fc1e132b7570205bda6fc5a8c5822101
georgetim1998 <georgetim1998@gmail.com>
2022-05-16 12:24:56 +0300

    cute figs

--- Spheromak_problem_params.py
+++ Spheromak_problem_params.py
@@ -1,23 +1,24 @@
+from cmath import sqrt
 import numpy
 import logger
 
 class Problem:
     def __init__(self):
         self.mesh_density = 50
-        self.default_mesh = 80
+        self.default_mesh = 150
         self.default_mesh_array = numpy.linspace(50, 950, 1+int((950-50)/50))
-        self.domain_boundary_coordinates = [0.05, 0.7, 
+        self.domain_boundary_coordinates = [0, 0.5, 
                                             -0.6, 0.6]
         
         self.psi_0 = 0.1
-        self.R = 0.3
+        self.R = 0.2*sqrt(2)
         self.alpha = 0.5
         
-        self.psi_min = -0.14
+        self.psi_min = -0.02
         self.psi_max = self.psi_0
-        self.psi_step = 0.02
+        self.psi_step = 0.01
         
-        # self.levels = 20
+        self.levels = 20
         self.levels = numpy.linspace(self.psi_min, self.psi_max, 1+int((self.psi_max-self.psi_min)/self.psi_step))
         
         self.errors = []
