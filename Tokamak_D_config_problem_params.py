b4e1035ed05a234405697778c62ddf75de92fd7a
georgetim1998 <georgetim1998@gmail.com>
2022-05-16 12:25:16 +0300

    cute figs

--- Tokamak_D_config_problem_params.py
+++ Tokamak_D_config_problem_params.py
@@ -6,17 +6,17 @@ class Problem:
         self.mesh_density = 50
         self.default_mesh = 500
         self.default_mesh_array = numpy.linspace(50, 950, 1+int((950-50)/50))
-        self.domain_boundary_coordinates = [0.02, 1.5, 
+        self.domain_boundary_coordinates = [0, 1.25, 
                                             -0.7, 0.7]
         
         self.psi_0 = 0.1
         self.eps = 0.05
         self.a = 1
-        self.b = 0.5
+        self.b = 0.4
         
-        self.psi_min = -1.5
-        self.psi_max = 9.5
-        self.psi_step = 0.5
+        self.psi_min = -1
+        self.psi_max = 9
+        self.psi_step = 1
         
         self.levels = 20
         self.levels = numpy.linspace(self.psi_min, self.psi_max, 1+int((self.psi_max-self.psi_min)/self.psi_step))
