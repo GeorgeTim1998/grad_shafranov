41d00e6257a24d81c1aa1e478d5a75a6e681a697
georgetim1998 <georgetim1998@gmail.com>
2022-05-26 13:32:17 +0300

    подготовка для сдвига источника

--- mu_vs_r_problem_params.py
+++ mu_vs_r_problem_params.py
@@ -3,20 +3,22 @@ import MEPHIST_data as M
 import logger
 import math
 import logger
+from funcs import M0
 
 
 class Problem:
     def __init__(self):
         self.domain_geometry = [0.05, 0.55, -0.4, 0.4]
-        self.mesh_density = 200
+        self.mesh_density = 120
 
 # %% Vessel info
         self.centre_point = [0.3, 0]
         self.ves_inner_radius = 0.1  # inner boundary info
-        self.ves_inner_segments = 60
+        self.ves_inner_segments = 100
 
         self.ves_outer_radius = 0.15  # outer boundary info
-        self.ves_outer_segments = 100
+        self.ves_outer_segments = int(
+            self.ves_outer_radius/self.ves_inner_radius * self.ves_inner_segments)
 
         self.area_ratio = 100*math.pi*(self.ves_outer_radius**2 - self.ves_inner_radius**2) / (
             self.domain_geometry[1]-self.domain_geometry[0]) / (self.domain_geometry[3]-self.domain_geometry[2])
@@ -26,17 +28,24 @@ class Problem:
         self.PLASMA_PERMEABILITY = 1
 
         self.VACUUM_CONDUCTIVITY = 0
-        self.VESSEL_CONDUCTIVITY = 7.2e-6
-        self.PLASMA_CONDUCTIVITY = 1e-6
+        self.VESSEL_CONDUCTIVITY = 1.4e6
+        self.PLASMA_CONDUCTIVITY = 9e6
 # %% Problem params
         self.psi_0 = 1e-3
         self.R = 0.2*math.sqrt(2)
         self.alpha = 0.5
 
+        self.ts = M0 * self.VESSEL_PERMEABILITY * self.VESSEL_CONDUCTIVITY * \
+            (self.ves_outer_radius - self.ves_inner_radius)**2
+            
         self.t0 = 0
-        self.t_max = 2e-15
-        num_of_t = 5+1
+        self.t_max = 10*self.ts
+        num_of_t = 2+1
         self.t = numpy.linspace(self.t0, self.t_max, num_of_t)
+
+        self.tm = 1*self.ts
+        self.R_t = (numpy.ones(len(self.t)) + self.t/self.tm)**4
+        
 
         self.boundary_condition_str = '0'
 # %% Plotting arrays and levels
@@ -57,6 +66,12 @@ class Problem:
 # %% Logs
         logger.info(message="t = linspace(%.4e, %.4e, %d)" %
                     (self.t0, self.t_max, int(num_of_t)))
+        logger.log_n_output_colored_message(
+            colored_message="ts = ", color='green', white_message=str(self.ts)
+        )
+        logger.log_n_output_colored_message(
+            colored_message="tm = ", color='green', white_message=str(self.tm)
+        )
 
         logger.log_n_output_colored_message(
             colored_message="VACUUM_PERMEABILITY = ", color='green', white_message=str(self.VACUUM_PERMEABILITY))
@@ -75,3 +90,4 @@ class Problem:
         logger.log_n_output_colored_message(
             colored_message="Area ratio: ", color='green', white_message=str(self.area_ratio)
         )
+        
