8e7d01ae9389f66f0a1fd374287d8b4798cc2616
georgetim1998 <georgetim1998@gmail.com>
2022-05-27 13:05:34 +0300

    moving point source

--- expressions.py
+++ expressions.py
@@ -2,6 +2,8 @@ from fenics import *
 import sympy
 import logger
 import funcs as fu
+from MEPHIST_data import MEPhIST
+import math
 
 M0 = 1.25e-6
 
@@ -44,14 +46,16 @@ class Expressions:
             colored_message="Displacement: ",
             color='green',
             white_message="disp_fact*vessl_rad*t/tm = %s*%s*%.3e/%.3e" % (str(problem.disp_fact), str(problem.ves_inner_radius), t, problem.tm))
-        fu.print_colored_n_white(colored_text='ts = ', color='green', white_text="%.3e" % problem.ts)
+        fu.print_colored_n_white(colored_text='ts = ',
+                                 color='green', white_text="%.3e" % problem.ts)
         logger.log_n_output_colored_message(
             colored_message="Displacement of source by: ", color='green', white_message=str(a))
         x = sympy.symbols('x[0]')
         z = sympy.symbols('x[1]')
 
-        # expr = psi0 * (x-a)**2 * (2*R**2 - (x-a)**2 - 4*alpha**2*z**2) / R**4
-        expr = psi0*(R**2 - (x-R-a)**2 - z**2) / R**2
+        expr = psi0 * (x-a)**2 * (2*R**2 - (x-a)**2 -
+                                  4*alpha**2*(z-0.1)**2) / R**4
+        # expr = psi0*(R**2 - (x-R-a)**2 - (z-0.3)**2) / R**2
 
         # first term (смотри формы оператора ГШ)
         f_expr_x1 = sympy.diff(sympy.diff(expr, x), x)
@@ -61,6 +65,36 @@ class Expressions:
         f_expr = -sympy.simplify(f_expr_x1 + f_expr_x2 + f_expr_z)
 
         f_text = sympy.printing.ccode(f_expr)
+
+        fu.print_colored(text='Moving source:', color='green')
+        sympy.pprint(f_expr)
+
+        return Expression(f_text, degree=2)
+
+    def moving_point_source(self, R, a, t, problem):
+        M = MEPhIST()
+
+        logger.log_n_output_colored_message(
+            colored_message="Displacement: ",
+            color='green',
+            white_message="disp_fact*vessl_rad*t/tm = %s*%s*%.3e/%.3e" % (str(problem.disp_fact), str(problem.ves_inner_radius), t, problem.tm))
+        fu.print_colored_n_white(colored_text='ts = ',
+                                 color='green', white_text="%.3e" % problem.ts)
+        logger.log_n_output_colored_message(
+            colored_message="Displacement of source by: ", color='green', white_message=str(a))
+
+        x = sympy.symbols('x[0]')
+        z = sympy.symbols('x[1]')
+
+        sigma = problem.ves_inner_radius*0.5
+        j0 = M.I/(math.pi*sigma**2)
+        logger.info(message="sigma = %.3e" % sigma)
+        logger.info(message="j0 = I/(pi*sigma**2) = %.3e" % j0)
+
+        f_expr = M0*x*j0 * sympy.exp(-((x-R-a)**2 + z**2) / sigma**2)
+
+        f_text = sympy.printing.ccode(f_expr)
+        f_text = f_text.replace('exp', 'std::exp')
 
         fu.print_colored(text='Moving source:', color='green')
         sympy.pprint(f_expr)
