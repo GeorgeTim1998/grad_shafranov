0f353f6cbfa437ca4c604ed0f224c9d5ba1098e7
georgetim1998 <georgetim1998@gmail.com>
2022-05-30 09:09:01 +0300

    good format for plots

--- for_base.py
+++ for_base.py
@@ -5,6 +5,12 @@ import datetime
 import matplotlib.pyplot as matplt
 from scipy.optimize import curve_fit
 
+FONT = {'family': "Times New Roman",
+        'size': 15}
+matplt.rc('font', **FONT)
+params = {'mathtext.default': 'regular' }          
+matplt.rcParams.update(params)
+
 def get_column(matrix, col):
     return [row[col] for row in matrix]
 
@@ -24,19 +30,20 @@ def plot_error_vs_mesh_from_file(folder_name, file_name, x_lim, PATH):
     
     popt, pcov = curve_fit(func1, numpy.log(mesh), numpy.log(error))    
     print(popt)
-    matplt.loglog(numpy.array(mesh), func2(numpy.array(mesh), *popt), linewidth=1)  # magnify w
-    matplt.loglog(mesh, error, 'o', linewidth=2)  # magnify w
+    matplt.loglog(numpy.array(mesh), func2(numpy.array(mesh), *popt), linewidth=1, label="$E_{max}$ ~ 1/$N_{\u2202\u03A9}^{2}$)")  # magnify w
+    matplt.loglog(mesh, error, 'o', linewidth=2, label="$E_{max}$($N_{\u2202\u03A9}$)")  # magnify w
+    matplt.legend()
     
     matplt.xlim(x_lim[0], x_lim[1])
     matplt.grid(True)
-    matplt.xlabel('Плотность сетки')
-    matplt.ylabel('Максимальная ошибка, Вб')
+    matplt.xlabel("$N_{\u2202\u03A9}$")
+    matplt.ylabel('$E_{max}$, Вб')
 
     save_contour_plot(PATH, '')
 
 def Time_name():
     ttime = datetime.datetime.now().strftime("%d%m%Y_%H%M%S")
-    time_title = str(ttime)  # get current time to make figure name unique
+    time_title = str(ttime) 
     return time_title
 
 def save_contour_plot(PATH, plot_title):
@@ -45,12 +52,11 @@ def save_contour_plot(PATH, plot_title):
     path_my_file = 'Figures/%s/%s' % (PATH, time_title)
     file_path = "%s.png" % path_my_file
 
-    matplt.title(plot_title)  # titled figure for my self
-    # no title figure for reports
+    matplt.title(plot_title) 
     matplt.savefig(file_path, dpi=240, bbox_inches="tight")
-    matplt.close()  # close created plot
+    matplt.close()  
 
     print("3D countour plot saved to PATH: %s" % file_path)
     time.sleep(1)
     
-plot_error_vs_mesh_from_file('Errors', '1D_problem_09052022_235717', [0, 1000], "SHOW_NOW")
+plot_error_vs_mesh_from_file('Errors', 'Soloviev_24012022_170000', [0, 1050], "SHOW_NOW")
